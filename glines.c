0d2521d0bb9f811896c5275adf81c6a618ebfa3f
Thomas Hindoe Paaboel Andersen <thomashpa@src.gnome.org>
2008-10-27 00:16:13 +0000

    move common help functionality to libgames-support/games-help.[ch]
    svn path=/trunk/; revision=8205

--- glines.c
+++ glines.c
@@ -39,6 +39,7 @@
 #include <libgames-support/games-files.h>
 #include <libgames-support/games-frame.h>
 #include <libgames-support/games-gridframe.h>
+#include <libgames-support/games-help.h>
 #include <libgames-support/games-preimage.h>
 #include <libgames-support/games-runtime.h>
 #include <libgames-support/games-scores.h>
@@ -1565,26 +1566,7 @@ game_quit_callback (GtkAction * action, gpointer data)
 static void
 game_help_callback (GtkAction * action, gpointer data)
 {
-  GdkScreen *screen;
-  GError *error = NULL;
-
-  screen = gtk_widget_get_screen (GTK_WIDGET (app));
-  gtk_show_uri (screen, "ghelp:glines", gtk_get_current_event_time (), &error);
-
-  if (error != NULL)
-  {
-    GtkWidget *d;
-    d = gtk_message_dialog_new (GTK_WINDOW (app), 
-                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                              GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, 
-                              "%s", _("Unable to open help file"));
-    gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (d),
-                              "             %s", error->message);
-    g_signal_connect (d, "response", G_CALLBACK (gtk_widget_destroy), NULL);
-    gtk_window_present (GTK_WINDOW (d));
-
-    g_error_free (error);
-  }
+  games_help_display (app, "glines", NULL);
 }
 
 static int
