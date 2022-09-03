3edc7e1c58ccf2b2d84eeab1ac453d5d829e7726
Matt Turner <mattst88@gmail.com>
2022-09-03 12:00:02 -0400

    build: Use GNOME module post_install()

--- meson_post_install.py
+++ meson_post_install.py
@@ -3,17 +3,6 @@
 import os
 import subprocess
 
-install_prefix = os.environ['MESON_INSTALL_PREFIX']
-icondir = os.path.join(install_prefix, 'share', 'icons', 'hicolor')
-schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
-
-if not os.environ.get('DESTDIR'):
-	print('Update icon cache...')
-	subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
-
-	print('Compiling gsettings schemas...')
-	subprocess.call(['glib-compile-schemas', schemadir])
-
 sourcedir = os.environ['MESON_SOURCE_ROOT']
 hooksdir = os.path.join(sourcedir, '.git', 'hooks')
 precommit = os.path.join(hooksdir, 'pre-commit')
